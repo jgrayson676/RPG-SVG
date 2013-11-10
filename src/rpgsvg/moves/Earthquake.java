7d32e1afc2f66ed1e5bfd98f4d82692d01ccf8e9
stevetran99 <stevetran.99@gmail.com>
2013-11-10 15:58:26 -0600

    Fixed? Earthquake

--- src/rpgsvg/moves/Earthquake.java
+++ src/rpgsvg/moves/Earthquake.java
@@ -3,15 +3,17 @@ package rpgsvg.moves;
 
 public class Earthquake extends Move {
 	
-	isPhysical = true;
-	type = 9;
-	damage = 100;
-	accuracy = 100;
-	priority = 0;
+	private boolean isPhysical = true;
+	private int type = 9;
+	private int damage = 100;
+	private int accuracy = 100;
+	private int priority = 0;
 	
 	private static final long serialVersionUID = -779779369788338150L;
 
 	public Earthquake(){
+		
+		
 		
 	}
 }
