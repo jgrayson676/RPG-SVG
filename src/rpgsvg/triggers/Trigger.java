76715f10fcb5c651931697c5edae8a570c3b771e
jgrayson676 <sleepingutensil@gmail.com>
2014-02-04 15:12:28 -0600

    Added String methods to Trigger

--- src/rpgsvg/triggers/Trigger.java
+++ src/rpgsvg/triggers/Trigger.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 import rpgsvg.Pokemon;
 
 public class Trigger implements Serializable{
-  private static final long serialVersionUID = 7L;
-  public Pokemon parent;
+	private static final long serialVersionUID = 7L;
+	public Pokemon parent;
 	int list = 0;
 	int counter;
 	boolean doesDisappear;
@@ -26,5 +26,22 @@ public class Trigger implements Serializable{
 		return list;
 	}
 	
+	public String getPokemon(){
+		return parent.name;
+	}
 	
+	public int getCounter(){
+		return counter;
+	}
+	
+	public String getList(){
+		switch(list){
+		case 1: return "Beginning of turn";
+		case 2: return "Before attack";
+		case 3: return "Before defence";
+		case 4: return "After defence";
+		case 5: return "End of turn";
+		}
+		return "No list found";
+	}
 }
