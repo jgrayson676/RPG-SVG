43d7f20c2d4892d933be01d8bc7321a23a66aaba
Steven Z. Chen <stevenzc@Stevens-MacBook-Air.local>
2014-02-26 09:54:09 -0600

    updated back image

--- src/rpgsvg/BackImage.java
+++ src/rpgsvg/BackImage.java
@@ -4,28 +4,25 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.JPanel;
 
+public class BackImage extends JPanel {
 
+	
+	private static final long serialVersionUID = 1L;
+	Image i;
 
-	public class BackImage extends JPanel {
-		 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		Image i;
-		 
-		//Creating Constructor
-		public BackImage(Image i) {
+	/**
+	 * A flip of the image put in.
+	 * @param i the image to be flipped
+	 */
+	public BackImage(Image i) {
 		this.i = i;
-		
-		}
-		 
-		//Overriding the paintComponent method
-		protected void paintComponent(Graphics g) {
+
+	}
+
+	// Overriding the paintComponent method
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(i, 0, 0, null);  // Drawing image using drawImage method
-		 
-		}
-		}
-	
-	
+		g.drawImage(i, 0, 0, null); // Drawing image using drawImage method
+
+	}
+}
